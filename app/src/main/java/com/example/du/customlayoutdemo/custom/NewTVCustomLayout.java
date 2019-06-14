bb09d8b0c0d7ea0f92249e609d5c541f38f3f79d
dusenmiao <du.senmiao@chinaott.net>
2019-06-14 12:29:30 +0800

    Revert "[add] 重构代码以符合现有逻辑"
    This reverts commit e20b3df8

--- app/src/main/java/com/example/du/customlayoutdemo/custom/NewTVCustomLayout.java
+++ app/src/main/java/com/example/du/customlayoutdemo/custom/NewTVCustomLayout.java
@@ -1,5 +1,6 @@
 package com.example.du.customlayoutdemo.custom;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.view.View;
